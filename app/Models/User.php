413b2505581f1cec768ae47e0f1f1fe5bfa346f6
su-min484 <85155408+su-min484@users.noreply.github.com>
2021-09-08 23:59:34 +0900

    MVC作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,12 +33,13 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class)->orderBy('order');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('tasks', 'TaskController@index')->name('tasks.index');
